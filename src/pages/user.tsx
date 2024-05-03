f950e85de9ca91aec5f9abd21622e35365505c4c
Afripaw <afripawdevelop@gmail.com>
2024-05-03 12:11:18 +0200

    feat: Updated user page

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1874,7 +1874,7 @@ const User: NextPage = () => {
         )}
         {(isCreate || isUpdate) && (
           <>
-            <div className="3xl:top-[8.5%] sticky z-50 flex justify-center md:top-[8.9%] xl:top-[11%]">
+            <div className="3xl:top-[8.5%] sticky z-50 flex justify-center md:top-[8.65%] xl:top-[11%]">
               <div className="relative mb-4 flex grow flex-col items-center rounded-lg bg-slate-300 px-5 py-6">
                 <b className=" text-2xl">{isUpdate ? "Update User Data" : "Create New User"}</b>
                 <div className="flex justify-center">
@@ -2423,7 +2423,7 @@ const User: NextPage = () => {
 
         {isViewProfilePage && (
           <>
-            <div className="3xl:top-[8.5%] sticky top-[11%] z-50 flex justify-center md:top-[8.9%] xl:top-[11%]">
+            <div className="3xl:top-[8.5%] sticky top-[11%] z-50 flex justify-center md:top-[8.65%] xl:top-[11%]">
               <div className="relative mb-4 flex grow flex-col items-center rounded-lg bg-slate-300 px-5 py-6">
                 <div className=" text-2xl">User Profile</div>
                 <div className="flex justify-center">
